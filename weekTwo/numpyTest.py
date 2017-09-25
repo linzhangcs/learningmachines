9c11d9dfda3406fdc9eeb1ed528385858b2344ae
Lin Zhang <linzhang@Lins-MacBook-Pro.local>
2017-09-25 01:04:37 -0400

    Add plots for K center and images and video files

--- weekTwo/numpyTest.py
+++ weekTwo/numpyTest.py
@@ -1,23 +1,49 @@
+"""
+=========================
+Simple animation examples
+=========================
+
+This example contains two animations. The first is a random walk plot. The
+second is an image animation.
+"""
+
 import numpy as np
+import matplotlib
+matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.animation as animation
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
-X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
-surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-ax.set_zlim(-1.01, 1.01)
 
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+def update_line(num, data, line):
+    line.set_data(data[..., :num])
+    return line,
 
-fig.colorbar(surf, shrink=0.5, aspect=5)
+fig1 = plt.figure()
+
+data = np.random.rand(2, 25)
+l, = plt.plot([], [], 'r-')
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.xlabel('x')
+plt.title('test')
+line_ani = animation.FuncAnimation(fig1, update_line, 25, fargs=(data, l),
+                                   interval=50, blit=True)
+line_ani.save('lines.mp4')
+# To save the animation, use the command: line_ani.save('lines.mp4')
+
+fig2 = plt.figure()
+
+x = np.arange(-9, 10)
+y = np.arange(-9, 10).reshape(-1, 1)
+base = np.hypot(x, y)
+ims = []
+for add in np.arange(15):
+    ims.append((plt.pcolor(x, y, base + add, norm=plt.Normalize(0, 30)),))
+
+im_ani = animation.ArtistAnimation(fig2, ims, interval=50, repeat_delay=3000,
+                                   blit=True)
+
+# To save this second animation with some metadata, use the following command:
+# im_ani.save('im.mp4', metadata={'artist':'Guido'})
 
 plt.show()
